6a282b4634e427d51326596dae06cf8e9f721c1c
Akash2Sharma <theakash@live.com>
2016-10-11 21:16:04 +0530

    UPDATE

--- Huffman.c
+++ Huffman.c
@@ -143,7 +143,7 @@ struct MinHeapNode* buildHuffmanTree(char data[], int freq[], int size)
 }
 // Prints huffman codes from the root of Huffman Tree.  It uses arr[] to store codes
 
-// A  C program for Huffman Coding
+
 void printCodes(struct MinHeapNode* root, int arr[], int top)
 {
         // Assign 0 to left edge and recur
@@ -185,4 +185,4 @@ int main()
     HuffmanCodes(arr, freq, size);
     return 0;
 }
-// A  C program for
+
