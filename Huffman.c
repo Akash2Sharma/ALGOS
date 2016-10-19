cfd97d143e9b20633a353f79cd5bfba882ed468e
Akash2Sharma <theakash@live.com>
2016-10-19 23:59:17 +0530

    UPDATE

--- Huffman.c
+++ Huffman.c
@@ -197,10 +197,10 @@ int main()
             scanf("%d", &n);
             int c = 0;
             while (n >= 5)
-                {
-                    n /= 5;
-                    c += n;
-                }                                          
+                { 
+                    n /= 5;  
+                    c += n; 
+                }                                           
             printf("%d\n", c);
         }     
     return 0;
